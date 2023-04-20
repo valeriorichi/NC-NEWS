864e6d75249d7d64054c44797c33ee8ae511268e
valeriorichi <valeriorichi@DESKTOP-H48B4SR>
2023-04-20 13:52:15 +0100

    Ticket 6

--- src/components/ArticlePage.jsx
+++ src/components/ArticlePage.jsx
@@ -1,11 +1,14 @@
 import { getArticle } from "../api";
+import CommentsExtension from "./CommentsExtension";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 
-const ArticlePage = () => {
+const ArticlePage = ({ loggedInUser }) => {
   const { article_id } = useParams();
   const query = `/${article_id}`;
   const [article, setArticle] = useState([{}]);
+  const [showComments, setShowComments] = useState(false);
+  const [updateCommentCount, setUpdateCommentCount] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
@@ -20,6 +23,10 @@ const ArticlePage = () => {
       });
   }, [query]);
 
+  const handleShowComments = () => {
+    setShowComments((prevState) => !prevState);
+  };
+
   if (isLoading) return <h3>Article is loading...</h3>;
 
   return (
@@ -31,8 +38,20 @@ const ArticlePage = () => {
         <h4>Created at: {article.created_at}</h4>
         <img src={article.article_img_url} alt={`${article.title}`} />
         <p>{article.body}</p>
-        <span>💙{article.votes}</span>
-        <span>💬{article.comment_count}</span>
+      </div>
+      <span>💙{article.votes}</span>
+      <span>💬{article.comment_count}</span>
+      <div>
+        <button onClick={handleShowComments}>💬{article.comment_count}</button>
+        {showComments ? (
+          <>
+            <CommentsExtension
+              article_id={article.article_id}
+              loggedInUser={loggedInUser}
+            />
+            <button onClick={handleShowComments}>Hide comments</button>
+          </>
+        ) : null}
       </div>
     </>
   );
--- src/components/CommentsExtension.jsx
+++ src/components/CommentsExtension.jsx
@@ -1,7 +1,39 @@
-import CommentForm from "./CommentForm";
+import { useEffect, useState } from "react";
+import { getCommentsWithAvatars } from "../api";
 
-const CommentsExtension = () => {
-  return <p>comment extension</p>;
+const CommentsExtension = ({ article_id }) => {
+  const [comments, setComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    setIsLoading(true);
+    getCommentsWithAvatars(article_id)
+      .then((response) => {
+        setComments(response);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [article_id]);
+
+  if (isLoading) return <h4>Comments are loading....</h4>;
+  return (
+    <>
+      {comments.map((comment) => (
+        <section key={comment.comment_id}>
+          <p>Author: {comment.author} </p>
+          <img
+            src={comment.author_avatar_url}
+            alt="Avatar"
+            width="50"
+            height="50"
+          />
+          <p>Posted at: {comment.created_at} </p>
+          <p>{comment.body} </p>
+        </section>
+      ))}
+    </>
+  );
 };
 
 export default CommentsExtension;
